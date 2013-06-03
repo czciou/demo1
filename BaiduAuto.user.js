9ff11a1052c74662cf789d0d277bfa5da318193f
czciou <czciou@163.com>
2013-06-03 19:01:52 +0800

    Signed-off-by: czciou <czciou@163.com>

--- BaiduAuto.user.js
+++ BaiduAuto.user.js
@@ -30,7 +30,7 @@
 // @include       http://www.zdomo.com/*
 // @grant       none
 // @require	https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
-// @version     1.3.13
+// @version     1.3.14
 // ==/UserScript==
 
 (function() {
